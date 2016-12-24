c326360ed744a166f4c3f2c389fc1f83fa3fa7f1
Juan Manuel Serrano <juanmanuel.serrano@hablapps.com>
2016-12-24 17:18:25 +0100

    consistent use of Σ

--- src/main/scala/Algebra.scala
+++ src/main/scala/Algebra.scala
@@ -2,15 +2,15 @@ package org.hablapps.azucar
 
 import cats.Functor
 
-trait Signature[F[_]]{
-  val F: Functor[F]
+trait Signature[Σ[_]]{
+  val F: Functor[Σ]
 }
 
 object Signature{
-  def apply[F[_]](implicit S: Signature[F]) = S
+  def apply[Σ[_]](implicit S: Signature[Σ]) = S
 }
 
-trait Algebra[F[_],X]{
-  val sig: Signature[F]
-  def apply(fx: F[X]): X
+trait Algebra[Σ[_],X]{
+  val sig: Signature[Σ]
+  def apply(fx: Σ[X]): X
 }
