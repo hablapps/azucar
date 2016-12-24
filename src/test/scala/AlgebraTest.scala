00a88473fef231a082ea58b8758bbcb543a211f7
Juan Manuel Serrano <juanmanuel.serrano@hablapps.com>
2016-12-24 15:23:48 +0100

    tests refactored

--- src/test/scala/AlgebraTest.scala
+++ src/test/scala/AlgebraTest.scala
@@ -4,45 +4,62 @@ import org.scalatest._
 
 import macros.algebra
 
-class AlgebraTest extends FlatSpec with Matchers {
-
-  import cats.derived._, functor._, legacy._
-  import cats.Functor
+class AlgebraTest extends FunSpec with Matchers {
 
   @algebra trait Monoid[A] {
     def mzero(): A
     def mappend(a1: A, a2: A): A
   }
 
-  val intMonoid = new Monoid[Int] {
-    def mzero() = 0
-    def mappend(a1: Int, a2: Int) = a1 + a2
+  def test(oalgebra: Monoid[Int], falgebra: Monoid.FAlgebra[Int]){
+    import falgebra.{Mzero, Mappend}
+
+    it("zero should match"){
+      oalgebra.mzero() shouldBe falgebra(Mzero())
+    }
+
+    it("mappend should match"){
+      oalgebra.mappend(1,2) shouldBe falgebra(Mappend(1,2))
+    }
   }
 
-  "Azucar" should "generate a type FAlgebra[A]" in {
-    type Whatever[A] = Monoid.FAlgebra[A]
+  describe("Generate F-algebras from O-algebras"){
+
+    val IntMonoidOAlgebra = new Monoid[Int] {
+      def mzero() = 0
+      def mappend(a1: Int, a2: Int) = a1 + a2
+    }
+
+    val IntMonoidFAlgebra: Monoid.FAlgebra[Int] =
+      Monoid.iso.to(IntMonoidOAlgebra)
+
+    test(IntMonoidOAlgebra, IntMonoidFAlgebra)
   }
 
-  it should "generate an associated ADT" in {
-    trait Whatever extends Monoid.FAlgebra[Int] {
+  describe("Generate O-algebras from F-algebras"){
+
+    val IntMonoidFAlgebra = new Monoid.FAlgebra[Int] {
       def apply(fx: InputF[Int]): Int = fx match {
         case Mzero() => 0
         case Mappend(i1, i2) => i1 + i2
       }
     }
+
+    val IntMonoidOAlgebra: Monoid[Int] =
+      Monoid.iso.from(IntMonoidFAlgebra)
+
+    test(IntMonoidOAlgebra, IntMonoidFAlgebra)
+
+
+    it("functor evidence should work"){
+      // import cats.derived._, functor._, legacy._
+      // import cats.Functor
+
+      import IntMonoidFAlgebra._
+
+      F.map(Mappend(3, 2))(_ * 2) shouldBe Mappend(6, 4)
+      F.map(Mzero[Int]())(_ + 1) shouldBe Mzero()
+    }
   }
 
-  it should "generate an isomorphism" in {
-    val falgebra = Monoid.iso.to(intMonoid)
-    import falgebra._
-    falgebra(Mappend(3, 2)) shouldBe 5
-    falgebra(Mzero()) shouldBe 0
-  }
-
-  it should "generate a functor evidence for InputF" in {
-    val falgebra = Monoid.iso.to(intMonoid)
-    import falgebra._
-    F.map(Mappend(3, 2))(_ * 2) shouldBe Mappend(6, 4)
-    F.map(Mzero[Int]())(_ + 1) shouldBe Mzero()
-  }
 }
